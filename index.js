8ee3c244b29bea000da16de908a47bffffddfe88
Taz Ahmed <87360462+tazahmed710@users.noreply.github.com>
2022-11-28 16:00:03 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const wbm = require("./wbm");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const path = require("path");
+//const path = require("path");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
