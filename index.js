c2294f8b9cae1d3c73bb3620e608a754cce22fc0
Taz Ahmed <87360462+tazahmed710@users.noreply.github.com>
2022-11-28 15:58:48 +0600

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("*", function (_, res) {
   res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
+    path.join(__dirname, "../client/build/index.html"),
     function (err) {
       if (err) {
         res.status(500).send(err);
